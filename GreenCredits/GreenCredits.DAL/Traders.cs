9de1f9babf854096f6a23381fed05c99ec10b6a4
unknown <prakashb@EXZINDLAP002.exzeo.net>
2015-09-14 12:31:13 +0530

    Added trader info

---
+++ GreenCredits/GreenCredits.DAL/Traders.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenCredits.DAL
+{
+    public class Traders
+    {
+        public long id { get; set; }
+        public string address { get; set; }
+        public string companyname { get; set; }
+        public string place { get; set; }
+        public string zip { get; set; }
+        public string sector { get; set; }
+        public string product { get; set; }
+        public string website { get; set; }
+        public string region { get; set; } 
+    }
+}
---
+++ GreenCredits/GreenCredits.Web/Helper.cs
@@ -0,0 +1,20 @@
+﻿using GreenCredits.DAL;
+using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace GreenCredits.Web
+{
+    public static class Helper
+    {
+        public static IHtmlString Traders(this HtmlHelper html)
+        {
+            var traders = ObjectFactory.GetInstance<IFarmerRepository>().GetTraders();
+            return html.Partial(@"~/Views/Dashboard/TradersListView.cshtml", traders);
+        }
+    }
+}
